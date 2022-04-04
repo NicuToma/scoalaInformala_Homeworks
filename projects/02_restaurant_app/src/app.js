8d0b9fbd3204e5b305c88d00aebee9837977c2b8
Toma Nicu <nikybv75fxp@yahoo.com>
2022-04-04 14:58:28 +0300

    restaurant app . html docs created and some css for home page and navbar

---
+++ projects/02_restaurant_app/src/app.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  let x = document.querySelector(".links");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+    x.style.gap = "10%";
+  }
+}
+
+// $(".carousel").carousel({
+//   interval: 2000,
+// });
